12ad20c7da148429a58ccd4e927d8325b27b7cd0
matiassarla <matiass2001@gmail.com>
2026-06-10 22:01:49 -0300

    Corrección carga de grupos

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ async function cargarEstudiantes() {
     const response = await fetch("https://backend-portafolioaapp.onrender.com/estudiantes");
     const data = await response.json();
 
-    grupos = data.grupos;
+    grupos = data;
     crearMenuGrupos();
     grupoActualIndex = 0;
 
